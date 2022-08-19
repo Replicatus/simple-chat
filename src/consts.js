ea88669f9f4edbf920ee67e754c943792929c507
RogozhinRA <ra.rogozhin@ined.ru>
2022-08-19 18:24:21 +0300

    some pages compplete and change styles for button without .text class

--- src/consts.js
+++ src/consts.js
@@ -1,11 +1,19 @@
 const fields = [
-    {text: 'Почта', name: 'email'},
-    {text: 'Логин', name: 'login'},
-    {text: 'Имя', name: 'second_name'},
-    {text: 'Фамилия', name: 'first_name'},
-    {text: 'Имя в чате', name: 'display_name'},
-    {text: 'Телефон', name: 'phone'},
+    {text: 'Почта', name: 'email', value: 'test@test.ru', disabled: 'disabled'},
+    {text: 'Логин', name: 'login', value: 'test value', disabled: 'disabled'},
+    {text: 'Имя', name: 'second_name', value: 'test value', disabled: 'disabled'},
+    {text: 'Фамилия', name: 'first_name', value: 'test value', disabled: 'disabled'},
+    {text: 'Имя в чате', name: 'display_name', value: 'test value', disabled: 'disabled'},
+    {text: 'Телефон', name: 'phone', value: '+ 7916161', disabled: 'disabled'},
+];
+const fieldsForPasswordPage = [
+    {text: 'Старый пароль',type: 'password', name: 'oldPassword', value: 'test@test.ru',},
+    {text: 'Новый пароль',type: 'password', name: 'newPassword', value: 'test value', },
+    {text: 'Повторите новый пароль',type: 'password', name: 'newPasswordConfirmed', value: 'test value'},
 ];
 export default {
-    fields
+    fields,
+    fieldsForPasswordPage,
+    changeUserProfile: false,
+    changeUserPassword: false,
 }
