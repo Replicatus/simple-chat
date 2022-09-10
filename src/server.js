2c05d594572c5159a1bcf6da9766d27aa7dc501f
Replicatus <rogozhin.ra@ined.ru>
2022-09-10 16:22:12 +0300

    add linters and eslint

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + './dist/'));
+app.use(express.static(`${__dirname}./dist/`));
 
 app.listen(PORT, (err) => {
-    if (err)
-        console.error("error", err);
-    console.log('express');
-    }
-);
+  if (err) {
+    // console.error('error', err);
+  }
+  // console.log('express');
+});
