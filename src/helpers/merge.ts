fc27707a2fa1d02b51ea6316a28b313a9a1479f0
Replicatus <rogozhin.ra@ined.ru>
2022-09-17 02:00:50 +0300

    add utils functions

---
+++ src/helpers/merge.ts
@@ -0,0 +1,194 @@
+type Indexed<T = unknown> = {
+    [key in string]: T;
+};
+
+function merge(lhs: Indexed, rhs: Indexed): Indexed {
+    const result: Indexed = lhs;
+    Object.entries(rhs).forEach(
+        ([key, value]) => {
+            if (Object.prototype.hasOwnProperty.call(result, key)) {
+                if (typeof value === 'object') {
+                    result[key] = merge(result[key] as Indexed, value as Indexed);
+                }
+            } else {
+                result[key] = value;
+            }
+        })
+
+    return result;
+}
+
+function set(object: Indexed | unknown, path: string, value: unknown): Indexed | unknown {
+    if (typeof path !== 'string')
+        throw new Error('path must be string');
+    if (typeof object !== 'object' || !object)
+        return object;
+    const newObj = path.split('.').reduceRight<Indexed>((acc, key) => ({
+        [key]: acc,
+    }), value as any);
+    return merge(object as Indexed, newObj);
+}
+
+type PlainObject<T = any> = {
+    [k in string]: T;
+};
+
+function isPlainObject(value: unknown): value is PlainObject {
+    return typeof value === 'object'
+        && value !== null
+        && value.constructor === Object
+        && Object.prototype.toString.call(value) === '[object Object]';
+}
+
+function isArray(value: unknown): value is [] {
+    return Array.isArray(value);
+}
+
+function isArrayOrObject(value: unknown): value is [] | PlainObject {
+    return isPlainObject(value) || isArray(value);
+}
+
+function isEqual(lhs: PlainObject, rhs: PlainObject) {
+    if (Object.keys(lhs).length !== Object.keys(rhs).length) {
+        return false;
+    }
+
+    for (const [key, value] of Object.entries(lhs)) {
+        const rightValue = rhs[key];
+        if (isArrayOrObject(value) && isArrayOrObject(rightValue)) {
+            if (isEqual(value, rightValue)) {
+                continue;
+            }
+            return false;
+        }
+
+        if (value !== rightValue) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function cloneDeep<T extends object = object>(obj: T) {
+    return (function _cloneDeep(item: T): T | Date | Set<unknown> | Map<unknown, unknown> | object | T[] {
+        // Handle:
+        // * null
+        // * undefined
+        // * boolean
+        // * number
+        // * string
+        // * symbol
+        // * function
+        if (item === null || typeof item !== "object") {
+            return item;
+        }
+
+        // Handle:
+        // * Date
+        if (item instanceof Date) {
+            return new Date(item.valueOf());
+        }
+
+        // Handle:
+        // * Array
+        if (item instanceof Array) {
+            let copy: PlainObject[] = [];
+
+            item.forEach((_, i) => (copy[i] = _cloneDeep(item[i])));
+
+            return copy;
+        }
+
+        // Handle:
+        // * Set
+        if (item instanceof Set) {
+            let copy = new Set();
+
+            item.forEach(v => copy.add(_cloneDeep(v)));
+
+            return copy;
+        }
+
+        // Handle:
+        // * Map
+        if (item instanceof Map) {
+            let copy = new Map();
+
+            item.forEach((v, k) => copy.set(k, _cloneDeep(v)));
+
+            return copy;
+        }
+
+        // Handle:
+        // * Object
+        if (item instanceof Object) {
+            let copy: Record<string | symbol, any> = {} ;
+
+            // Handle:
+            // * Object.symbol
+            // @ts-ignore
+            Object.getOwnPropertySymbols(item).forEach((s: symbol) => (copy[s] = _cloneDeep(item[s])));
+
+            // Handle:
+            // * Object.name (other)
+            // @ts-ignore
+            Object.keys(item).forEach((k : string) => (copy[k] = _cloneDeep(item[k])));
+
+            return copy;
+        }
+
+        throw new Error(`Unable to copy object: ${item}`);
+    })(obj);
+}
+// type StringIndexed = Record<string, any>;
+//
+// function getPairs (obj : StringIndexed, keys: unknown[] = []) : any[]{
+//     return Object.entries(obj).reduce((pairs, [key, value])=> {
+//         if (typeof value === 'object')
+//             pairs.push(...getPairs(value, [...keys, key]));
+//         else
+//             pairs.push([[...keys, key], encodeURIComponent(String(value))]);
+//         return pairs;
+//     }, [] as any[])
+// }
+//
+// function queryStringify(obj : StringIndexed) : string | never {
+//     if (!isPlainObject(obj))
+//         throw new Error('input must be an object');
+//     const result = getPairs(obj)
+//         .map(([[key0, ...keysRest], value]) =>
+//             `${key0}${keysRest.map((a: string) => `[${a}]`).join('')}=${value}`)
+//         .join('&');
+//     return result
+// }
+
+function getKey(key: string, parentKey?: string) {
+    return parentKey ? `${parentKey}[${key}]` : key;
+}
+
+function getParams(data: PlainObject | [], parentKey?: string) {
+    const result: [string, string][] = [];
+
+    for(const [key, value] of Object.entries(data)) {
+        if (isArrayOrObject(value)) {
+            result.push(...getParams(value, getKey(key, parentKey)));
+        } else {
+            result.push([getKey(key, parentKey), encodeURIComponent(String(value))]);
+        }
+    }
+
+    return result;
+}
+
+function queryString(data: PlainObject) {
+    if (!isPlainObject(data)) {
+        throw new Error('input must be an object');
+    }
+
+    return getParams(data).map(arr => arr.join('=')).join('&');
+}
+
+export {set, merge, isEqual, cloneDeep, queryString}
+
+
