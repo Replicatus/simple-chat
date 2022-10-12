87305027371f2868f7ed61a01380c2e3a4193e5a
Replicatus <rogozhin.ra@ined.ru>
2022-10-12 08:49:50 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// app.use(express.static(`${__dirname}./dist/`));
 app.use('*', (req, res) => {
   res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
