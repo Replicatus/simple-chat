a0001f5a8ce0b33741928b95c095c39bde0770c1
Replicatus <rogozhin.ra@ined.ru>
2022-10-12 20:08:28 +0300

    fix server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fallback = require('express-history-api-fallback');
+const path = require('path');
 const app = express();
 const PORT = 3000;
-const root = `${__dirname}/dist`;
+const root = path.resolve(__dirname, '../dist/');
 app.use(express.static(root))
 app.use(fallback('index.html', { root: root }))
 
